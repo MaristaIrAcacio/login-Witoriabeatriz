071a198885d81e772f26fd72e8a137861930234d
witoria santos <witoria.santos@alunoescolassociais.org.br>
2023-07-25 12:13:00 -0300

    Operação da função de editar e excluir nome

--- login.js
+++ login.js
@@ -1,29 +1,44 @@
-//Bloco de verificação de preenchimento de campos
-function Verifica(){
+// BLOCO DE VERIFICAÇÃO DE PREENCHIMENTO DE CAMPOS
+function Verifica() {
     let user = document.getElementById('loginEmail').value;
     let senha = document.getElementById('loginSenha').value;
-    
-    if(!user || !senha){
+
+    if (!user || !senha) {
         alert("Campos de preenchimento obrigatório. Favor preencher!");
 
     } else {
         window.location.href = "aula2.html";
-     }}
+    }
+}
 
-      var dadosLista = [];
-      function salvarUser(){
+var dadosLista = [];
+function salvarUser() {
     let nomeUser = document.getElementById("nomeUser").value;
-    if(nomeUser){
+    if (nomeUser) {
         dadosLista.push(nomeUser);
         crialista();
-    //console.log(dadosLista)
- }}
- function crialista(){
-    let tabela = document.getElementById("tabela").innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
-     for(let i=0; i<= (dadosLista.length-1); i++){
-    tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-success' onclick=''>Editar</button><button class='btn btn-danger' onclick=''>Excluir</button></td></tr>";
-    document.getElementById('tabela').innerHTML = tabela;
+        document.getElementById("nomeUser").value = '';
+        //console.log(dadosLista)
     }
-     
- }
-    
+}
+function crialista() {
+    let tabela = document.getElementById("tabela").innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
+    for (let i = 0; i <= (dadosLista.length - 1); i++) {
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-success' onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button class='btn btn-danger' onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
+
+        document.getElementById('tabela').innerHTML = tabela;
+    }
+}
+//Função para a editar o array
+
+function editar(i) {
+    document.getElementById('nomeUser').value = dadosLista[(i - 1)];
+    dadosLista.splice(dadosLista[(i - 1)], 1);
+}
+
+//Função para excluir array
+
+function excluir(i) {
+    dadosLista.splice((i - 1), 1);
+    document.getElementById("tabela").deleteRow(i);
+}
